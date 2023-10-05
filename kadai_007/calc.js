7f0d85ff983e8d9fce96d843af922d5b0be7d906
takamori minami <minami@takamorinoMacBook-Pro.local>
2023-10-05 15:48:12 +0900

    JavaScript_kadai_007-02

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -14,17 +14,17 @@ let num = 8;
 */
 
 // if条件式
+// [変数(num)が3と5の倍数の場合]
+if (num % 15 == 0){
+  console.log("3と5の倍数です");
+}
 // [変数(num)が3の倍数の場合]
-if (num % 3 == 0){
+else if (num % 3 == 0){
   console.log("3の倍数です");
 }
 // [変数(num)が5の倍数の場合]
 else if (num % 5 == 0){
   console.log("5の倍数です");
-}
-// [変数(num)が3と5の倍数の場合]
-else if (num % 3 == 0 && num % 5 == 0){
-  console.log("3と5の倍数です");
 }
 // [上記以外の場合]
 else {
